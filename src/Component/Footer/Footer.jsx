e31d8e4523e6ce1532be4ba018028640ccdbe5ee
Sohel Dayma <soheldayma1@gmail.com>
2024-09-09 00:15:43 +0530

    first commit

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+    <p>@ 2024 Educity. All right reserved.</p>
+    <ul>
+        <li>Terms of service</li>
+        <li>Privacy policy</li>
+    </ul>
+      
+    </div>
+  )
+}
+
+export default Footer
